201164d08844d60858ed0777fba67dd3ab281c1a
reus <reusee@gmail.com>
2021-11-07 01:20:44 +0800

    add Comparable; optimize Compare

--- compare.go
+++ compare.go
@@ -1,8 +1,33 @@
 package it
 
-import "github.com/reusee/sb"
+import (
+	"strings"
+
+	"github.com/reusee/sb"
+)
+
+type Comparable interface {
+	Cmp(b any) int
+}
 
 func Compare(a, b any) int {
+	switch a := a.(type) {
+	case string:
+		if b, ok := b.(string); ok {
+			return strings.Compare(a, b)
+		}
+	case uint8:
+		if b, ok := b.(uint8); ok {
+			if a < b {
+				return -1
+			} else if a > b {
+				return 1
+			}
+			return 0
+		}
+	case Comparable:
+		return a.Cmp(b)
+	}
 	return sb.MustCompare(
 		sb.Marshal(a),
 		sb.Marshal(b),
